351281565365dde72e00f9c1f0905f229cc28b31
J0hnnIoo <johnny.mrb@hotmai.com>
2024-07-18 22:21:49 -0300

    feat(funcoes.php):Inseri mais funcuões que verificam se a senha atende os requisitos

--- php/funcoes.php
+++ php/funcoes.php
@@ -2,12 +2,44 @@
     function verificaMaisculaeMinuscula($senha) {
         if (ctype_lower($senha) && ctype_upper($senha)) {
             return true;
-        }
+        } else return false;
+        
     }
 
-    function verificaSeTem3Numeros($senha) {
-        if (count_chars(ctype_alnum($senha))) {
+    function verificaSeTemEspecial($senha) {
+        if (ctype_punct($senha)) {
             return true;
-        }
+        } else return false;
     }
+
+    function verificaSeTem8caracter($senha) {
+        if (count_chars($senha) >= 8) {
+            return true;
+        } else return false;
+    }
+
+    function verificaSeTem3numeros($senha) {
+        $arr1 = str_split($senha);
+        $counter = 0;
+        foreach ($arr1 as $caracter) {
+            if (ctype_digit($caracter)) {
+                $counter++;
+            }
+        }
+        
+        if ($counter >= 3) {
+            return true;
+        } else return false;
+    }
+
+    function validaSenha($senha) {
+        if (verificaMaisculaeMinuscula($senha) 
+            && verificaSeTemEspecial($senha) 
+            && verificaSeTem8caracter($senha) 
+            && verificaSeTem3numeros($senha)) {
+            return true;
+        } else return false;
+    }
+
+    
 ?>
