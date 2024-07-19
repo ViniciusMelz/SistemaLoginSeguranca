887fd0d1a0a348c81f5de05b5cd30e4d148d8cb2
J0hnnIoo <johnny.mrb@hotmai.com>
2024-07-18 21:32:53 -0300

    feat(funcoes.php): criação das funções que verificam se tem 3 numeros e se tem maiscula e minuscula

--- php/funcoes.php
+++ php/funcoes.php
@@ -1,3 +1,13 @@
 <?php
+    function verificaMaisculaeMinuscula($senha) {
+        if (ctype_lower($senha) && ctype_upper($senha)) {
+            return true;
+        }
+    }
 
+    function verificaSeTem3Numeros($senha) {
+        if (count_chars(ctype_alnum($senha))) {
+            return true;
+        }
+    }
 ?>
