b8c0861fa77dd20a95568970b482286b879255a7
ViniciusMelz <viniciusgmelz@gmail.com>
2024-07-27 13:52:22 -0300

    feat(loginRealizado): criada nova pagina para quando o login é autorizado, tendo 2 botoes, um para trocar a senha e outro para sair, caso a senha atual tenha mais de 45 dias o site pede para troca-la

---
+++ php/loginRealizado.php
@@ -0,0 +1,46 @@
+<?php
+require_once ("funcoes.php");
+session_start();
+$email = $_SESSION['email'];
+$senha = $_SESSION['senha'];
+
+$query = "SELECT id 
+          FROM Usuario 
+          WHERE email = '$email' 
+          AND senha = '$senha'";
+$result = mysqli_query(conectarBanco(), $query);
+
+$row = mysqli_fetch_array($result);
+$_SESSION['id_usuario'] = $row[0];
+$queryDiferencaDatas = "SELECT DATEDIFF(SYSDATE(), data_criacao) 
+                        FROM senhas_antigas 
+                        WHERE id_usuario = '$row[0]' 
+                        ORDER BY id DESC 
+                        LIMIT 1";
+$resultDiferencaDatas = mysqli_query(conectarBanco(), $queryDiferencaDatas);
+$rowDiferencaDatas = mysqli_fetch_array($resultDiferencaDatas);
+$diferencaDatas = $rowDiferencaDatas[0];
+
+if ($diferencaDatas > 45) {
+    $text = 'Você já está com a mesma senha a mais de 45 dias, pedimos que troque a senha!';
+}else{
+    $text = 'Logado com Sucesso!';
+}
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1><?php echo $text ?></h1>
+    <a href="trocaSenha.php"><button>Trocar Senha</button></a>
+    <a href="../index.php"><button>Sair</button></a>
+</body>
+
+</html>
